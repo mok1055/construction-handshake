c82e586a3d2ea9bd5aededdcce60eed98d02a6d2
Mohamed Kichouhi <mohamed.kichouhi@student.hu.nl>
2019-11-08 12:46:06 +0100

    Login and dashboard combined

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,21 +1,65 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    <title>{{ config('app.name') }}</title>
+    <title>Construction Handshake</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="{{ asset('images/icons/favicon.ico') }}" />
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('fonts/iconic/css/material-design-iconic-font.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('vendor/animate/animate.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('vendor/css-hamburgers/hamburgers.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('vendor/animsition/css/animsition.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('vendor/select2/select2.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('vendor/daterangepicker/daterangepicker.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/util.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/main.css') }}">
+    <!--===============================================================================================-->
 </head>
 
 <body>
-    @if(isset(Auth::user()->email))
-    <div class="alert alert-danger success-block">
-        <h3>Welcome {{ Auth::user()->name }}</h3>
-        <a href="{{ url('/logout') }}">Logout</a>
+    <div class="topbar">
+        <div class="header-info">
+            @if(isset(Auth::user()->email))
+            <h3>Welcome {{ Auth::user()->name }}</h3>
+            <a href="{{ url('/logout') }}">Logout</a>
+        </div>
+        @else
+        <script>
+            window.location = "/home";
+        </script>
+        @endif
     </div>
-    @else
-    <script>
-        window.location = "/home";
-    </script>
-    @endif
-</body>
+    <!-- The sidebar -->
+    <div class="sidebar">
+        <a class="active" href="#home">Dashboard</a>
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
+    </div>
 
-</html>
+    <!-- Page content -->
+    <div class="content">
+        <div class="planning">
+            <h2>Projecten</h2>
+        </div>
+        <div class="agenda">
+            <h2>Agenda</h2>
+        </div>
+        <div class="contact">
+            <h2>Contact</h2>
+        </div>
+    </div>
+</body>
