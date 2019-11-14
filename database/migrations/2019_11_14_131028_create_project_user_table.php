d4f7fe482556e217e92b23c700729d916d32de33
Mohamed Kichouhi <mohamed.kichouhi@student.hu.nl>
2019-11-14 15:41:20 +0100

    Database migration added based off ERD

---
+++ database/migrations/2019_11_14_131028_create_project_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_user', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('project_id');
+            $table->foreign('user_id')->references('user_id')->on('user');
+            $table->foreign('project_id')->references('project_id')->on('project');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_user');
+    }
+}
