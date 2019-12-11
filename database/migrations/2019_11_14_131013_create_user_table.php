4aa6d4959cbd568c2275e125451327f8774a9641
Mohamed Kichouhi <mohamed.kichouhi@student.hu.nl>
2019-12-11 16:03:22 +0100

    A user now has role and with that, a user role table is also added

--- database/migrations/2019_11_14_131013_create_user_table.php
+++ database/migrations/2019_11_14_131013_create_user_table.php
@@ -15,12 +15,17 @@ class CreateUserTable extends Migration
     {
         Schema::create('user', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('role_id')->default(1);
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email')->unique();
             $table->string('password');
             $table->timestamp('creation_date')->useCurrent();
             $table->timestamps();
+
+            $table->foreign('role_id')
+                ->references('id')
+                ->on('user_role');
         });
     }
 
