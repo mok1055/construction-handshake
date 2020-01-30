212f9a77cd5567f30a451f53709df87aea6e3794
Mohamed Kichouhi <mohamed.kichouhi@student.hu.nl>
2020-01-30 14:58:27 +0100

    Rating finished

--- resources/views/view-ratings.blade.php
+++ resources/views/view-ratings.blade.php
@@ -1,5 +1,29 @@
 @extends('layouts.app')
 
+@section('sidebar')
+    <div class="sidebar">
+        <ul class="sidebar-list">
+            <li><a href="{{ url('dashboard') }}">Home</a></li>
+            <li><a href="{{ url('profile') }}">Mijn profiel</a></li>
+            <li><a href="{{ url('projects') }}">Projecten overzicht</a></li>
+            @if (Auth::user()->canCreateEditProject())
+                <li>
+                    <a href="{{ url('projects/create') }}" class="{{ (Request::is('projects/create')) ? 'active' : '' }}">Project toevoegen</a>
+                </li>
+            @endif
+            <li><a href="{{ url('agenda') }}">Agenda</a></li>
+            <li><a href="{{ Auth::user()->canCreateEditProject() ? route('projects.edit', $project->id) : route('projects.show', $project->id) }}">Project: {{ $project->name }}</a></li>
+            <li><a href="{{ url('projects/'.$project->id.'/view-users/') }}">Personen overzicht</a></li>
+            @if ($project->canRate(Auth::user()))
+                <li><a href="{{ url('projects/'.$project->id.'/ratings/create') }}">Beoordeling</a></li>
+            @endif
+            @if (Auth::user()->canViewRatings())
+                <li><a href="{{ url('projects/'.$project->id.'/ratings/') }}" class="active">Beoordelingen overzicht</a></li>
+            @endif
+        </ul>
+    </div>
+@endsection
+
 @section('content')
     <h3>Beoordeling overzicht</h3><br>
     <table class="projects">
@@ -9,7 +33,7 @@
             <th>Achternaam</th>
             <th>Cijfer</th>
             <th>Opmerkingen</th>
-            <th>Afbeelding</th>
+            <th></th>
         </tr>
         @foreach($ratings as $rating)
             <tr>
@@ -18,7 +42,9 @@
                 <td>{{$rating->user()->last_name}}</td>
                 <td>{{$rating->mark}}</td>
                 <td>{{$rating->comments}}</td>
-                <td><img src="{{ Storage::url('app/images/ratings/'.$rating->image_path)}}"></td>
+                <td><button onclick="location.href='{{ url('projects/'.$rating->project_id.'/ratings/'.$rating->id) }}'">
+                        Weergeven
+                    </button></td>
             </tr>
         @endforeach
     </table>
